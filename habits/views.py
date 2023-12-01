b879ad78964021482c908447a75c3226ba9929cf
Azzlem <azzlem.sid@google.com>
2023-12-01 20:20:58 +0300

    написал документацию

--- habits/views.py
+++ habits/views.py
@@ -9,6 +9,7 @@ from habits.services import make_task, delete_task
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
+
     permission_classes = (IsAuthenticated,)
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
@@ -26,10 +27,13 @@ class HabitCreateAPIView(generics.CreateAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = HabitsSerializer
 
+    @swagger_auto_schema(operation_summary="Создание привычки")
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
     def perform_create(self, serializer, **kwargs):
         """
-        Сохраняет авторизованного пользователя в объекте привычки,
-        создает периодическую задачу если привычка не является приятной.
+        Сохраняет авторизованного пользователя в привычку, создает периодическую задачу.
         """
 
         new_habit = serializer.save()
@@ -44,8 +48,12 @@ class HabitListAPIView(generics.ListAPIView):
     serializer_class = HabitsSerializer
     pagination_class = HabitPaginator
 
+    @swagger_auto_schema(operation_summary="Список привычек")
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
     def get_queryset(self):
-        """ Получает только привычки владельца. """
+        """ Получает привычки юзера. """
 
         queryset = Habits.objects.filter(user=self.request.user)
         return queryset
@@ -57,17 +65,33 @@ class HabitPublicListAPIView(generics.ListAPIView):
     serializer_class = HabitsSerializer
     pagination_class = HabitPaginator
 
+    @swagger_auto_schema(operation_summary="Список публичных привычек")
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
 
 class HabitRetrieveAPIView(generics.RetrieveAPIView):
     permission_classes = (IsAuthenticated, IsOwnerOrStaff,)
     queryset = Habits.objects.all()
     serializer_class = HabitsSerializer
 
+    @swagger_auto_schema(operation_summary="Выбранная привычка")
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
     permission_classes = (IsAuthenticated, IsOwnerOrStaff,)
     queryset = Habits.objects.all()
     serializer_class = HabitsSerializer
+
+    @swagger_auto_schema(operation_summary="Изменение привычки")
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary="Изменение привычки")
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
 
     def perform_update(self, serializer):
         """ Удаляет и создает периодическую задачу с новыми данными. """
@@ -81,6 +105,10 @@ class HabitDestroyAPIView(generics.DestroyAPIView):
     permission_classes = (IsAuthenticated, IsOwnerOrStaff,)
     queryset = Habits.objects.all()
     serializer_class = HabitsSerializer
+
+    @swagger_auto_schema(operation_summary="Удаление привычки")
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
     def perform_destroy(self, instance):
         """ Удаляет периодическую задачу. """
